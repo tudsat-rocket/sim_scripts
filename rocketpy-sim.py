84ad5b974ad49a99d53d4db49a723167bcec438e
Luis Wientgens <windgans@protonmail.com>
2024-06-19 16:05:13 +0200

    Simulation is running, some parameters still placeholders

--- rocketpy-sim.py
+++ rocketpy-sim.py
@@ -45,43 +45,47 @@ height = 3.015
 mass = 15.245
 radius=0.075
 
-# Regarding calculation of intertia c.f. https://itp.uni-frankfurt.de/~luedde/Lecture/Mechanik/Intranet/Skript/Kap7/node5.html
+# Regarding calculation of inertia c.f. https://itp.uni-frankfurt.de/~luedde/Lecture/Mechanik/Intranet/Skript/Kap7/node5.html
 frodo_m = Rocket(
     radius=radius,
     mass=mass,
     inertia=((mass/2)*0.5*(radius**2+(height**2/3)), (mass/2)*0.5*(radius**2+(height**2/3)), (mass/2)*radius**2),
     power_off_drag="data/drag/FrodoMPowerOffDrag.csv",
     power_on_drag="data/drag/FrodoMPowerOnDrag.csv",
-    center_of_mass_without_motor=0,
+    center_of_mass_without_motor=0, # origin is at center of dry mass
     coordinate_system_orientation="tail_to_nose"
 )
 
+'''
+TODO: ADD
 buttons = frodo_m.set_rail_buttons(
     upper_button_position=0, #TODO
     lower_button_position=0, #TODO
 )
+'''
 
 frodo_m.add_motor(motor_m1790, 
-        position=0 #TODO
+        position=-1.297
         )
 
 nose_cone = frodo_m.add_nose(
-    length=0, #TODO,
+    length=0.4,
     kind="von karman",
-    position=0, #TODO
+    position=1.653, 
 )
 
 #TODO
 fin_set = frodo_m.add_trapezoidal_fins(
-    n=4,
+    n=3,
     root_chord=0.120,
     tip_chord=0.060,
-    span=0.110,
-    position=-1.04956,
+    span=0.150,
+    position=-1.044,
     cant_angle=0.5,
-    airfoil=("../data/calisto/NACA0012-radians.csv","radians"),
+    airfoil=None, # can we get airfoil parameters?
 )
 
+'''
 # TODO
 tail = frodo_m.add_tail(
     top_radius=0.0635,  
@@ -89,6 +93,7 @@ tail = frodo_m.add_tail(
     length=0.060, 
     position=-1.194656
 )
+'''
 
 main = frodo_m.add_parachute(
     name="main",
