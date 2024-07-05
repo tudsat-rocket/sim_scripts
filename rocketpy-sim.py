f27cfc2adaac83da0f07f04774b3d9525d7c55f7
Manuel Schlüsener <manuel.schluesener@tudsat.space>
2024-07-05 13:57:49 +0200

    Change coordinate system, swap motor, edit shape

--- rocketpy-sim.py
+++ rocketpy-sim.py
@@ -17,24 +17,24 @@ launch_environment.set_date(
 launch_environment.set_atmospheric_model(
     type="custom_atmosphere",
     pressure=None,
-    temperature=273.15+21,
+    temperature=273.15+30,
     wind_u = [(0,-1.5), (1000, -5)],
     wind_v = [(0,-0.5), (1000, -2)],
 )
 
-motor_m1790 = SolidMotor(
-    thrust_source="data/motors/Cesaroni_8088M1790-P.eng",
-    dry_mass=3.567,
+motor_m1545 = SolidMotor(
+    thrust_source="data/motors/Cesaroni_8187M1545-P.eng",
+    dry_mass=3.043,
     dry_inertia=(0.125, 0.125, 0.002),#sample values, idk
-    center_of_dry_mass_position=0.3, #just a bad guess
-    grains_center_of_mass_position=0.35, #an even worse guess
-    burn_time=4.5,
-    grain_number = 4,
-    grain_separation=0.005,#idk
+    center_of_dry_mass_position=0.5, #just a bad guess
+    grains_center_of_mass_position=0.5, #an even worse guess
+    burn_time=5.3,
+    grain_number = 6,
+    grain_separation=0.001,#idk
     grain_density=1815,#idk
-    grain_outer_radius=0.046,
-    grain_initial_inner_radius=0.020,
-    grain_initial_height=0.13,
+    grain_outer_radius=0.035,
+    grain_initial_inner_radius=0.010,
+    grain_initial_height=0.16,
     nozzle_radius=0.039,
     throat_radius=0.013,
     interpolation_method="linear",
@@ -42,8 +42,8 @@ motor_m1790 = SolidMotor(
 )
 
 height = 3.015
-mass = 15.245
-radius=0.075
+mass = 17.245
+radius=0.076
 
 # Regarding calculation of inertia c.f. https://itp.uni-frankfurt.de/~luedde/Lecture/Mechanik/Intranet/Skript/Kap7/node5.html
 frodo_m = Rocket(
@@ -52,48 +52,47 @@ frodo_m = Rocket(
     inertia=((mass/2)*0.5*(radius**2+(height**2/3)), (mass/2)*0.5*(radius**2+(height**2/3)), (mass/2)*radius**2),
     power_off_drag="data/drag/FrodoMPowerOffDrag.csv",
     power_on_drag="data/drag/FrodoMPowerOnDrag.csv",
-    center_of_mass_without_motor=0, # origin is at center of dry mass
-    coordinate_system_orientation="tail_to_nose"
+    center_of_mass_without_motor=1.54, #coordinate system same as OpenRocket, RasAero
+    coordinate_system_orientation="nose_to_tail"
 )
 
-'''
-TODO: ADD
+
 buttons = frodo_m.set_rail_buttons(
-    upper_button_position=0, #TODO
-    lower_button_position=0, #TODO
+    upper_button_position=1.90, #TODO
+    lower_button_position=2.95, #TODO
 )
-'''
 
-frodo_m.add_motor(motor_m1790, 
-        position=-1.297
+
+frodo_m.add_motor(motor_m1545, 
+        position=3.21
         )
 
 nose_cone = frodo_m.add_nose(
     length=0.4,
     kind="von karman",
-    position=1.653, 
+    position=0,
 )
 
-#TODO
+
 fin_set = frodo_m.add_trapezoidal_fins(
     n=3,
-    root_chord=0.25,
-    tip_chord=0.15,
+    root_chord=0.24,
+    tip_chord=0.16,
     span=0.150,
-    position=-1.044,
-    sweep_length=0.1,
-    airfoil=None, # can we get airfoil parameters?
+    position=2.87,
+    sweep_length=0.06,
+    airfoil=None #TODO can we get airfoil parameters?
 )
 
-'''
+
 # TODO
 tail = frodo_m.add_tail(
-    top_radius=0.0635,  
-    bottom_radius=0.0435,
-    length=0.060, 
-    position=-1.194656
+    top_radius=0.076,  
+    bottom_radius=0.06,
+    length=0.10, 
+    position=3.11
 )
-'''
+
 
 main = frodo_m.add_parachute(
     name="main",
